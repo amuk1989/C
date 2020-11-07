632734f9720b7e3de45c59d6ae8f18df443393e0
amuk1989 <58622904+amuk1989@users.noreply.github.com>
2020-11-08 03:55:15 +0700

    Добавьте файлы проекта.

---
+++ Main.c
@@ -0,0 +1,17 @@
+#include "Main.h"
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+
+	struct Human
+	{
+		int age;
+		float mass;
+		float height;
+	};
+
+	//inner
+	
+
+	return 0;
+}
